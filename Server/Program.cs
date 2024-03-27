8efff9b704abd2449bd53c70500844f1f203399f
owenboy9 <owenkraus9@gmail.com>
2024-03-27 15:13:52 +0100

    Cleaned out git cache and commited inital code
    cleaned out the cache in order to make sure future commits are clean
    and don't have a bunch of binaries that can be generated on build.

--- Server/Program.cs
+++ Server/Program.cs
@@ -19,7 +19,7 @@ try
     
 
     app.MapGet("/", Greet);
-    //app.MapGet("/users", AllUsers);
+    app.MapGet("/users", Users.All);
 
     app.Run();
 
@@ -32,40 +32,6 @@ catch (MySqlException e)
 finally
 {
     db?.Close();
-}
-
-
-string AllUsers(State state)
-{
-    string result = string.Empty;
-    string query = "SELECT * FROM users";
-    MySqlCommand command = new(query, state.DB);
-
-    using MySqlDataReader reader = command.ExecuteReader();
-
-    while (reader.Read())
-    {
-        int id = reader.GetInt32("id");
-        string username = reader.GetString("username");
-        result += $"{username} has the id: {id}\n";
-    }
-    return result;
-}
-
-string Login(User credentials, Dictionary<string, string> state)
-{
-    if (credentials.Email == "manuel@nodehill.com" && credentials.Password == "password123")
-    {
-        state.Remove("signed-in");
-        state.Add("signed-in", "true");
-        return "Signed in\n";
-    }
-    else
-    {
-        state.Remove("signed-in");
-        state.Add("signed-in", "false");
-        return "Not signed in\n";
-    }
 }
 
 
