25b9d342b091d2e95345e1304bb9a1ba2bfcea13
owenboy9 <owenkraus9@gmail.com>
2024-03-27 14:25:08 +0100

    gitignore

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441d21ffb14e32970b504b0097dc447651eca678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e045fbc9918788eea526d8446eaf9db6cfd224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
